886647aa28d63ca3617c5c500b0bcf6294d69686
Yati Myint Lwin <yatimyintlwin43@gmail.com>
2024-01-03 23:45:59 +0630

    [Added] Vue Login Form Exercise

--- vue-sample/src/App.vue
+++ vue-sample/src/App.vue
@@ -3,7 +3,9 @@ import ExampleThree from './components/examples/ExampleThree.vue'
 import ExampleOne from './components/examples/ExampleOne.vue'
 import ExampleTwo from './components/examples/ExampleTwo.vue'
 import ExampleFour from './components/examples/ExampleFour.vue'
+
 import ExerciseOne from './components/exercises/ExerciseOne.vue'
+import ExerciseTwo from './components/exercises/ExerciseTwo.vue'
 </script>
 
 <template>
@@ -13,6 +15,7 @@ import ExerciseOne from './components/exercises/ExerciseOne.vue'
   <ExampleFour />
 
   <ExerciseOne />
+  <ExerciseTwo />
 </template>
 
 <style scoped lang="scss"></style>
---
+++ vue-sample/src/components/exercises/ExerciseTwo.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const result = ref('')
+const inputUserName = ref('')
+const inputPassword = ref('')
+
+function onLogin() {
+  if (inputUserName.value === 'admin' && inputPassword.value === 'admin') {
+    result.value = 'correct'
+  } else {
+    result.value = 'incorrect'
+  }
+}
+</script>
+
+<template>
+  <h1>Login</h1>
+  <label for="username">Enter username: </label>
+  <input type="text" id="username" v-model="inputUserName" />
+  <br /><br />
+
+  <label for="password">Enter password: </label>
+  <input type="password" id="password" v-model="inputPassword" />
+  <br /><br />
+
+  <button @click="onLogin">Login</button>
+
+  <div v-if="result !== ''">
+    <h3 v-if="result === 'correct'" id="success">Login successful!</h3>
+    <h3 v-else id="fail">username or password is worng!</h3>
+  </div>
+</template>
+
+<style scoped lang="scss">
+#success {
+  color: green;
+}
+
+#fail {
+  color: red;
+}
+</style>
