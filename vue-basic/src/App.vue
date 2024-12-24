a4fda61c59ecb84b82e9dee5bfa0991d54a556be
yatimyintlwin <yatimyintlwin43@gmail.com>
2024-12-24 16:57:55 +0630

    [Add] Vue Exercises

---
+++ vue-basic/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <ExampleOne />
+
+  <MyClock />
+</template>
+
+<script setup lang="ts">
+import ExampleOne from './components/ExampleOne.vue'
+
+import MyClock from './components/MyClock.vue'
+</script>
+
+<style scoped lang="scss"></style>
---
+++ vue-basic/src/components/ExampleOne.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h1 :class="redColor">{{ greet }}</h1>
+    <br />
+    <p>Count: {{ count }}</p>
+    <button @click="increment">Increment</button>
+    <button @click="customIncrese(2)">Double Increse</button>
+    <button @click="customIncrese(3)">Triple Increse</button>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const greet = ref('Hi Jhon!')
+const redColor = ref('red-color')
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+
+function customIncrese(num: number) {
+  count.value += num
+}
+</script>
+
+<style scoped lang="scss">
+.red-color {
+  color: red;
+}
+</style>
