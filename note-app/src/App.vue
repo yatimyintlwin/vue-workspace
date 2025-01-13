8364d51e272780d7a96fc54155cd010656dac101
yatimyintlwin <yatimyintlwin43@gmail.com>
2025-01-13 20:50:44 +0630

    [Add] My Note App

---
+++ note-app/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>
+    <a-divider style="height: 2px; background-color: #7cb305" />
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
